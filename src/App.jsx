28daa19c095eda21dc0f56fb951a93a545f7c929
Frankin Angarita <128552142+Frankin16@users.noreply.github.com>
2023-08-13 00:20:19 -0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-import PokedexPage from './pages/pokedexPage'
+import PokedexPage from './pages/PokedexPage'
 import PokeIdPage from './pages/PokeIdPage'
 import Page404 from './pages/Page404'
 import ProtectedRoutes from './pages/ProtectedRoutes'
@@ -14,8 +14,8 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<HomePage/>} />
-          <Route element={<ProtectedRoutes/>} >
-          <Route path='/pokedex' element={<PokedexPage/>} />
+            <Route element={<ProtectedRoutes/>} >
+            <Route path='/pokedex' element={<PokedexPage/>} />
           <Route path='/pokedex/:id' element={<PokeIdPage/>} />
           </Route>
           <Route path='*' element={<Page404/>} />
